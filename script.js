1dc6f6fbfd5d3ddcaf1094c89aefd640b5bdad3a
Kauã Camargo Ribeiro <kauacamargoribeiro@gmail.com>
2024-08-07 01:59:35 -0300

    ok

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.getElementById('payment-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    const cpfInput = document.getElementById('cpf').value.trim();
+    const keyInput = document.getElementById('key').value.trim();
     const amountInput = parseFloat(document.getElementById('amount').value);
 
-    // CPF e valor esperados
-    const expectedCpf = '127.726.329-97';
+    // Chave e valor esperados
+    const expectedKey = '6f8bbd14-4a74-984c-323e7efa6ecd';
     const expectedAmount = 29.99;
 
-    if (cpfInput === expectedCpf && amountInput === expectedAmount) {
+    if (keyInput === expectedKey && amountInput === expectedAmount) {
         document.getElementById('course-content').classList.remove('hidden');
         document.getElementById('payment-form').reset();
         alert('Curso desbloqueado com sucesso! Você pode acessar o grupo no WhatsApp abaixo.');
     } else {
-        alert('Pagamento não identificado ou valor incorreto. Verifique o CPF e o valor e tente novamente.');
+        alert('Pagamento não identificado ou valor incorreto. Verifique a chave e o valor e tente novamente.');
     }
 });
